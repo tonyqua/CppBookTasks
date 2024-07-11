1078e632d5e97f7c5ea66b60ce8be0bc318a1342
TonyQ <150294902+tonyqua@users.noreply.github.com>
2024-07-11 22:32:35 +0300

    Update task18.cpp

--- chapter1/task18.cpp
+++ chapter1/task18.cpp
@@ -3,22 +3,26 @@
 const int size = 20;
 int arr[size];
 
+void arrayFunc();
+
+void print();
+
+int main() {
+	arrayFunc();
+	return 0;
+}
+
 void arrayFunc() {
 	arr[0] = 0;
 	arr[1] = 1;
 	for (int i = 2; i < size; ++i) {
 		arr[i] = arr[i - 1] + arr[i - 2];
 	}
+	print();
 }
 
 void print() {
 	for (int i = 0; i < size; ++i) {
 		std::cout << arr[i] << " ";
 	}
-}
-
-int main() {
-	arrayFunc();
-	print();
-	return 0;
 }
